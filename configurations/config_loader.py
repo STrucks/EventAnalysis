017860d1fe4fae02504a0b7273f77a9050489e38
Christopher Strucks <chr.strucks@gmail.com>
2020-05-28 20:49:55 +0200

    - initial commit

---
+++ configurations/config_loader.py
@@ -0,0 +1,9 @@
+import json
+
+
+class ConfigLoader:
+    """
+    This class loads all configurations and stores them in a config_store
+    """
+    def __init__(self):
+        self.config_score = {}
